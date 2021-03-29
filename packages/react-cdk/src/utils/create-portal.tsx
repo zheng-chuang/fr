109a3a4163b2cc53c2dcda9ff91dca454bb7d250
zhengchuangchuang <zhengchuangchuang@kuaishou.com>
2021-03-29 16:26:09 +0800

    fix: portal remove children

--- packages/react-cdk/src/utils/create-portal.tsx
+++ packages/react-cdk/src/utils/create-portal.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export function createPortal() {
-  let el: Element = document.createElement("div");
-
+  const dom: Element = document.createElement("div");
+  let el = dom;
   const portal: React.FC<{ visible: boolean; dom?: Element }> = (props) => {
     React.useEffect(() => {
+      if (el.parentElement) return;
       document.body.appendChild(el);
       return () => {
+        if (!el.parentElement) return;
         document.body.removeChild(el);
       };
     }, []);
 
     React.useEffect(() => {
-      if (!props.dom) return;
-      el = props.dom;
+      if (!props.dom) {
+        el = dom;
+      } else {
+        el = props.dom;
+      }
+      return () => {
+        el = dom;
+      };
     }, [props.dom]);
 
     return props.visible ? ReactDOM.createPortal(props.children, el) : null;
